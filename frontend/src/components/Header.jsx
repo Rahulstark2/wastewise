2cacbf763838a569ecfacc9d703fd42d905bd98b
Rahulstark2 <rahulstark2@gmail.com>
2024-08-25 00:08:21 +0530

    Added some styling in navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate, useLocation } from 'react-router-dom'; // Import useLocation
 import TopHeader from './TopHeader';
 import logo from '../assets/image.png';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Header = () => {
-  const navigate = useNavigate(); // Use the useNavigate hook
+  const navigate = useNavigate();
+  const location = useLocation(); // Get current location
+
+  // Function to check if the current path is active
+  const isActive = (path) => location.pathname === path;
 
   return (
     <div>
@@ -19,12 +23,39 @@ const Header = () => {
           </div>
         </div>
         <nav className="flex flex-row items-center space-x-4 mt-7 md:mt-0">
-          <button onClick={() => navigate("/home")} className="text-custom-black">Home</button>
-          <button onClick={() => navigate("/services")} className="text-custom-black">Our Services</button>
-          <button onClick={() => navigate("/blog")} className="text-custom-black">Blog</button>
-          <button onClick={() => navigate("/contact")} className="text-custom-black">Contact Us</button>
+          <button
+            onClick={() => navigate("/home")}
+            className={`text-custom-black pb-1 border-b-4 ${
+              isActive("/home") ? 'border-custom-olive-green' : 'border-transparent'
+            } hover:border-custom-olive-green transition duration-300`}
+          >
+            Home
+          </button>
+          <button
+            onClick={() => navigate("/services")}
+            className={`text-custom-black pb-1 border-b-4 ${
+              isActive("/services") ? 'border-custom-olive-green' : 'border-transparent'
+            } hover:border-custom-olive-green transition duration-300`}
+          >
+            Our Services
+          </button>
+          <button
+            onClick={() => navigate("/blog")}
+            className={`text-custom-black pb-1 border-b-4 ${
+              isActive("/blog") ? 'border-custom-olive-green' : 'border-transparent'
+            } hover:border-custom-olive-green transition duration-300`}
+          >
+            Blog
+          </button>
+          <button
+            onClick={() => navigate("/contact")}
+            className={`text-custom-black pb-1 border-b-4 ${
+              isActive("/contact") ? 'border-custom-olive-green' : 'border-transparent'
+            } hover:border-custom-olive-green transition duration-300`}
+          >
+            Contact Us
+          </button>
         </nav>
-
       </header>
     </div>
   );
