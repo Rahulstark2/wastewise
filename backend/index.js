e5e6765b86a7594b70f209e2353fb266e4d837c6
Rahulstark2 <rahulstark2@gmail.com>
2024-08-26 08:28:06 +0530

    Fixed cors bug on backend

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,20 @@ const cors = require("cors");
 const mainRouter = require("./routes/index");
 
 const app = express();
+const allowedOrigins = ['http://localhost:5174', 'http://localhost:5173','https://wastewise-sigma.vercel.app/'];
 
-app.use(cors());
+app.use(cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+  
 app.use(express.json());
 
 app.use("/api/v1",mainRouter);
